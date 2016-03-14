3d7bd2fd58d6d9cd285a81a4977e97ebc57b6d8e
erinef <erinef@comcast.net>
2016-03-14 16:48:16 -0400

    update to weather data pull

--- pull_weather_data.R
+++ pull_weather_data.R
@@ -3,24 +3,24 @@ date = gsub("-","/",todaydate)
 startdate = as.Date("2015/01/01")
 
 firstpart = "http://www.wunderground.com/history/airport/EGFF/"
-lastpart = "/DailyHistory.html?req_city=Cardiff&req_state=&req_statename=
-            United+Kingdom&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=03717&format=1"
-##weather_data = read.csv(paste(firstpart,date,lastpart))
+lastpart = "/DailyHistory.html?req_city=Cardiff&req_state=&req_statename=United+Kingdom&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=03717&format=1"
 
-for (i in startdate:todaydate) {
+##weather_data = read.csv(paste(firstpart,date,lastpart)) ##works
+
+for (i in 0:(todaydate-startdate)) {  
   
-  wind_data = read.csv(paste(firstpart,startdate,lastpart))
+  weather_data = read.csv(paste(firstpart,(startdate+i),lastpart,sep="")) ##runs but doesn't work--weird format
   
 
-  x <- wind_data$DateUTC.br...
+  x <- weather_data$DateUTC
   
-  
-  y <-wind_data$Wind.SpeedMPH
+  y <-weather_data$Wind.SpeedMPH
   
   jpeg(file="~/SURGE/web/Wind Speed ")
   plot(x,y, ylab="Wind Speed (mph)", xlab="Date", main="Wind Speed in 30 min Intervals in Cardiff,UK")
   dev.off()
-  startdate = startdate + 1
   
+  ## "need finite 'xlim' values" ?
 }
+
 
