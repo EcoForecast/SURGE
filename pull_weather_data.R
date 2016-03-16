1889e90e1348bcaeb6d3fdf83c8f65b8d1754bc3
erinef <erinef@comcast.net>
2016-03-16 16:21:35 -0400

    finished weather cron job

--- pull_weather_data.R
+++ pull_weather_data.R
@@ -5,22 +5,30 @@ startdate = as.Date("2015/01/01")
 firstpart = "http://www.wunderground.com/history/airport/EGFF/"
 lastpart = "/DailyHistory.html?req_city=Cardiff&req_state=&req_statename=United+Kingdom&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=03717&format=1"
 
-##weather_data = read.csv(paste(firstpart,date,lastpart)) ##works
-
+##weather_data = read.csv(paste(firstpart,date,lastpart)) 
+weather_data = list()
 for (i in 0:(todaydate-startdate)) {  
   
-  weather_data = read.csv(paste(firstpart,(startdate+i),lastpart,sep="")) ##runs but doesn't work--weird format
-  
-
-  x <- weather_data$DateUTC
-  
-  y <-weather_data$Wind.SpeedMPH
-  
-  jpeg(file="~/SURGE/web/Wind Speed ")
-  plot(x,y, ylab="Wind Speed (mph)", xlab="Date", main="Wind Speed in 30 min Intervals in Cardiff,UK")
-  dev.off()
+  date2 = gsub("-","/", startdate+i)
+  weather_data[[i+1]] = read.csv(paste(firstpart,date2,lastpart,sep="")) 
   
   ## "need finite 'xlim' values" ?
 }
 
+wind = unlist(sapply(weather_data,function(x){x$Wind.SpeedMPH},simplify = TRUE))
+pres = unlist(sapply(weather_data,function(x){x$Sea.Level.PressureIn},simplify = TRUE))
+pres[pres < 5] = NA
+dateUTC = unlist(sapply(weather_data,function(x){x$DateUTC},simplify = TRUE))
 
+day = strptime(sub(pattern = "<br />","",as.character(dateUTC)),format="%Y-%m-%d %T")
+
+#met = Reduce(function(...) merge(...,all=TRUE),weather_data)
+
+jpeg(file="~/SURGE/web/WindSpeed.jpg")
+     plot(day,wind, ylab="Wind Speed (mph)", xlab="Date", main="Wind Speed in 30 min Intervals in Cardiff,UK",type='l')
+     dev.off()
+     
+jpeg(file="~/SURGE/web/SeaLevelPressure.jpg")
+          plot(day,pres, ylab="Sea Level Pressure (in)", xlab="Date", main="Sea Level Pressure in 30 min Intervals in Cardiff,UK",type='l')
+dev.off()
+          
