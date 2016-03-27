064f2b42f11fe319689a0bccdd0fa0a3fad7b31a
BrennaS <brennas@bu.edu>
2016-03-27 13:43:20 -0400

    updated model, a few questions in notes

--- random_historic.R
+++ random_historic.R
@@ -15,19 +15,26 @@ for(i in 1:n){
 wind.effect[i] ~ dnorm(wind[i],tau_wind)
 }
 
+for(i in 1:n){
+pressure_effect[i] ~dnorm(pressure[i], tau_presssure)
+}
+
 #### Process Model
 for(i in 2:n){
-##final total will be???= total <- x[i] + beta*wind.effect[i]*((9.8/y[i])*(25+pressure)) + tau_add
-total <- x[i] + wind.effect[i] + tau_add
+total <- x[i] + (beta * wind.effect[i] ) * ((9.8/y[i])*(25 + pressure_effect[i])) + tau_add
 x[i]~dnorm(x[i-1],tau_add)##change to how surge changes
 }
 
 #### Priors
 x[1] ~ dunif(-1,10)
-tau_obs ~ dgamma(a_obs,r_obs)
-tau_add ~ dgamma(a_add,r_add)
-tau_wind ~ dgamma(1,1)
-wind[1] ~ dnorm(0,2##need SD around wind.effect, not wind speed)
+tau_obs ~ dgamma(a_obs,r_obs) ##observation error for tide height
+tau_add ~ dgamma(a_add,r_add) ##process error
+tau_wind ~ dgamma(1,1) ##obervation error for wind measurements
+wind[1] ~ dnorm(0,3) ##uninformative prior for wind ##3 makes sense w/ units?
+pressure[1] ~ dunif(0,5) ##uninformative prior for pressure
+  ##what are pressure units, does prior make sense
+tau_pressure ~dgamma(1,1) ##obs error for pressure measurement
+##all taus unknown, so went with same as given taus
 }
 "
 data <- list(y=y,n=length(y),a_obs=1,r_obs=1,a_add=1,r_add=1)
