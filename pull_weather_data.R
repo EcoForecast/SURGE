d1f9eeb8d11ec83d0323e9adf391ad6ac10a0f87
“BrennaS” <brennas@bu.edu>
2016-04-07 15:57:41 -0500

    functional* weather pulls

--- pull_weather_data.R
+++ pull_weather_data.R
@@ -8,7 +8,9 @@ lastpart = "/DailyHistory.html?req_city=Cardiff&req_state=&req_statename=United+
 
 ##weather_data = read.csv(paste(firstpart,date,lastpart)) 
 weather_data = list()
-for (i in 0:(todaydate-startdate)) {  
+load("weather_data.Rdata")
+start = i
+for (i in start:(todaydate-startdate)) {  
   print(i)
   date2 = gsub("-","/", startdate+i)
   met.url = paste(firstpart,date2,lastpart,sep="")
@@ -32,7 +34,7 @@ jpeg(file="~/SURGE/web/WindSpeed.jpg")
      dev.off()
      
 jpeg(file="~/SURGE/web/SeaLevelPressure.jpg")
-          plot(day,pres, ylab="Sea Level Pressure (in)", xlab="Date", main="Sea Level Pressure in 30 min Intervals in Cardiff,UK",type='l')
-dev.off()
+     plot(day,pres, ylab="Sea Level Pressure (in)", xlab="Date", main="Sea Level Pressure in 30 min Intervals in Cardiff,UK",type='l')
+     dev.off()
 
           
