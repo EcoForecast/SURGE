3af8ac066f9e1b4e5f79cd817a283c3564c84e82
BrennaS <brennas@bu.edu>
2016-04-08 12:37:10 -0400

    took out the overlap, praying it fixes the problem

--- pull_surge_data.R
+++ pull_surge_data.R
@@ -16,7 +16,6 @@ x<- strptime(dtod, format="%Y-%m-%d %H:%M:%S")
 
 surgeAndTide <-as.numeric(levels(height$f)[height$f])
 
-
 jpeg(file="~/SURGE/web/Tide+Surge_Height_until_Now.jpg")
 plot(x,surgeAndTide, ylab="Tide Height (m)", xlab="Date", main="Tide+Surge height at 15 minute intervals in Newport,UK")
 dev.off()
