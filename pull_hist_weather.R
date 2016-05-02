0e050cca18218991ed842cce82357d0d9833d072
“BrennaS” <brennas@bu.edu>
2016-05-02 11:08:00 -0500

    weather data file and hist weather pull

--- pull_hist_weather.R
+++ pull_hist_weather.R
@@ -6,8 +6,8 @@ firstpart = "https://www.wunderground.com/history/airport/EGFF/"
 lastpart = "/DailyHistory.html?req_city=Cardiff&req_state=&req_statename=United+Kingdom&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=03717&format=1"
 
 ##weather_data = read.csv(paste(firstpart,date,lastpart)) 
-hist_weather_data2 = list()
-load("hist_weather_data2.Rdata")
+hist_weather_data = list()
+load("hist_weather_data.Rdata")
 start = i
 for (i in start:(enddate-startdate)) {  
   
@@ -16,15 +16,15 @@ for (i in start:(enddate-startdate)) {
   met.url = paste(firstpart,date2,lastpart,sep="")
   # system(paste0("cd met; wget ",met.url))
   x = getURL(met.url)
-  hist_weather_data2[[i+1]] = read.csv(text=x) 
-  save(i,hist_weather_data,file="hist_weather_data2.Rdata") 
+  hist_weather_data[[i+1]] = read.csv(text=x) 
+  save(i,hist_weather_data,file="hist_weather_data.Rdata") 
   
 }
 
-hist_wind = unlist(sapply(hist_weather_data2,function(x){x$Wind.SpeedMPH},simplify = TRUE))
-hist_pres = unlist(sapply(hist_weather_data2,function(x){x$Sea.Level.PressureIn},simplify = TRUE))
+hist_wind = unlist(sapply(hist_weather_data,function(x){x$Wind.SpeedMPH},simplify = TRUE))
+hist_pres = unlist(sapply(hist_weather_data,function(x){x$Sea.Level.PressureIn},simplify = TRUE))
 hist_pres[hist_pres < 5] = NA
-dateUTC = unlist(sapply(hist_weather_data2,function(x){x$DateUTC},simplify = TRUE))
+dateUTC = unlist(sapply(hist_weather_data,function(x){x$DateUTC},simplify = TRUE))
 
 day = strptime(sub(pattern = "<br />","",as.character(dateUTC)),format="%Y-%m-%d %T")
 
